34b3cb785c5f3a96661ca208c8568f7c6a279180
Dr. Dillinger <peter.dillinger@srh.de>
2022-11-16 15:51:49 +0100

    Programm sollte soweit fertig sein!

--- src/Kofferpacken.java
+++ src/Kofferpacken.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Kofferpacken {
     public static void main(String[] args) {
-        
+        Scanner eingabe = new Scanner(System.in);
+        String koffer = "";
+        String item;
+
+        do {
+            System.out.println("Was packst du ein? (gib 'fertig' ein zum Beenden)");
+            item = eingabe.next();
+
+            if ( ! item.equals("fertig") ) {
+                if (!koffer.isEmpty()) koffer = koffer + ", ";
+                koffer = koffer + item;
+            }
+            System.out.println("Kofferinhalt: " + koffer);
+        } while ( ! item.equals("fertig") );
+        System.out.println("Koffer fertig gepackt. Ab geht's in den Urlaub");
     }
 }
